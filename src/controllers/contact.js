2e1a627c34a26a6b5ad63a7d2d1afe5b4b90d51b
MohammadAlhalaq <m-a-s-h@outlook.com>
2019-08-07 15:42:51 +0300

    add html form and create rout contact and install kill-port and and models Relates #17

---
+++ src/controllers/contact.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const { join } = require('path');
+
+exports.postContact = (req, res, next) => {
+  fs.appendFile(join(__dirname, '..', 'models', 'contacts.json'), JSON.stringify(req.body), (err) => {
+    if (err) next(err);
+  });
+  res.status(304).redirect('/');
+//   res.render('home', { title: 'Your Gallery' });
+};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const { get } = require('./home');
 const { client, server } = require('./error');
 const { getPhoto } = require('./data');
+const { postContact } = require('./contact');
 
 const router = express.Router();
 
 router.get('/', get);
 router.get('/photo', getPhoto);
+router.post('/contact', postContact);
 
 router.use('*', client);
 router.use(server);
